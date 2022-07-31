01dc72932ded652086397da8a27ad2b92dd4abe8
Dave Stevens <hello@davestevens.co.uk>
2022-07-31 18:17:21 +0100

    Fixes service worker path.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const app = new App({ target: document.body });
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
