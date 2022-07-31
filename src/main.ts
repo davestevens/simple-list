4509367619d43eef54a541fe65a18cc4af055b7d
Dave Stevens <hello@davestevens.co.uk>
2022-07-31 17:10:35 +0100

    Adds PWA.

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ import "./global.css";
 
 const app = new App({ target: document.body });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const preprocess = require('svelte-preprocess');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const mode = process.env.NODE_ENV ?? 'development';
 const isProduction = mode === 'production';
@@ -53,9 +54,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
     }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
     new CopyPlugin({
       patterns: [
         { from: path.resolve(__dirname, 'src/img'), to: path.resolve(__dirname, 'build/img') },
+        { from: path.resolve(__dirname, 'src/manifest.webmanifest'), to: path.resolve(__dirname, 'build') },
       ],
     }),
   ],
